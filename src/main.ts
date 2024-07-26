44b7bc415672f29a56830e9422205976f12bf60d
Lucas Mendes <lucas.mendessilva@hotmail.com>
2024-07-26 00:19:55 -0300

    Correção de importação no arquivo main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './exceptions/globalExceptionFilter';
+import { AllExceptionsFilter } from './exception/globalExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
